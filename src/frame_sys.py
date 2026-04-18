26a81947f7f416b4badd718a75118886801310f6
Wiebe Beeftink <wipeautcrafter@gmail.com>
2026-04-18 16:51:46 +0200

    fix: remove shlex

--- src/frame_sys.py
+++ src/frame_sys.py
@@ -1,5 +1,4 @@
 import subprocess
-from shlex import quote
 
 DJO_URL = "https://docs.google.com/presentation/d/e/2PACX-1vQJBiddvZZDcDDwfcJYjOHc3PNARrlw4m5IlrwLgu61_yyKbdObbicb_Q3sU5-O5WliC2KJ_HzdCjQr/pubembed?rm=minimal&start=true&loop=true&delayms=15000"
 BITLAIR_URL = "https://bitlair.nl/Hoofdpagina"
@@ -12,7 +11,7 @@ class FrameSys:
             raise ChildProcessError("Subprocess exited with non-zero code.")
 
     def _set_url(self, url: str):
-        self._run(["snap", "set", "chromium", f"url={quote(url)}"])
+        self._run(["snap", "set", "chromium", f"url={url}"])
         self._run(["snap", "restart", "chromium"])
 
     def show_djo(self):
